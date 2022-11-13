771212ca4b71d086a7ca2a25b5ae385dbd066ebd
정관용 <rhks7410dyd@naver.com>
2022-11-13 05:17:16 +0000

    copy other person's code. need to study later

--- BFS/boj11967.cpp
+++ BFS/boj11967.cpp
@@ -1,4 +1,58 @@
-#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+using pii = pair<int,int>;
+int n, m, light[101][101], visited[101][101], canMove[101][101];
+struct Info { int x, y; };
+vector <Info> room[101][101];
+int dx[] = {0,0,1,-1};
+int dy[] = {1,-1,0,0};
+int bfs(){
+    int ans = 0;
+    visited[1][1] = 1;
+    light[1][1] = 1;
+    queue <pii> q;
+    q.push({1,1});
+    while(!q.empty()){
+        int x = q.front().first;
+        int y = q.front().second;
+        q.pop();
+        for(auto &r : room[x][y]) light[r.x][r.y] = 1; 
+
+        for(int i = 0; i < 4; i++){
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+            if(1 > nx || nx > n || 1 > ny || ny > n) continue;
+            canMove[nx][ny] = 1;
+        }
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= n; j++){
+                if(!light[i][j] || visited[i][j] || !canMove[i][j]) continue;
+                visited[i][j] = 1;
+                q.push({i,j});
+            }
+        }
+    }
+
+    for(int i = 1; i<=n; i++){
+        for(int j = 1; j <=n;j++){
+            if(light[i][j]) ans++;
+        }
+    }
+
+    return ans;
+}
+
+int main(){
+    cin >> n >> m;
+    while(m--){
+        int x,y,a,b;
+        cin >> x >> y >> a >> b;
+        room[x][y].push_back({a,b});
+    }
+    cout << bfs();
+}
+
+/*#include <iostream>
 #include <queue>
 
 using namespace std;
@@ -56,6 +110,7 @@ void BFS(){
     
     q.push({1,1});
     lightup[1][1] = true;
+	visit[1][1] = true;
     link[1][1] = true;
     light_up_room++;
     while(!q.empty()){
@@ -68,7 +123,7 @@ void BFS(){
             if(link[ny][nx])    continue;
             light_up_room++;
             lightup[ny][nx] = true;
-            if(adjacent(ny,nx)){//이 파트를 쭉쭉 이어나가서 주변에 추가할 곳이 없을 때까지 해야됨!
+            if(adjacent(ny,nx)){
                 link[ny][nx] = true;
                 visit[ny][nx] = true;
                 q.push({ny,nx});
@@ -104,3 +159,4 @@ int main()
     return 0;
 }
 
+*/
