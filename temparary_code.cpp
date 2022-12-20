20acc3eb3b5fcd11b373059ad6987eeafea37b91
정관용 <rhks7410dyd@naver.com>
2022-12-20 14:30:04 +0000

    unsolved problem

--- temparary_code.cpp
+++ temparary_code.cpp
@@ -1,25 +1,33 @@
 #include <iostream>
-#include <string>
-#include <vector>
 
 using namespace std;
+
+//수학적으로 먼저 나누면 안되는 건가? 머리가 안돌아감.
 
 int main(){
 	cin.tie(NULL);
 	cout.tie(NULL);
 	ios::sync_with_stdio(false);
 	
-	vector<double> v;
+	int L;
+	long long hash_weight[51];
+	string input;
+	cin >> L >> input;
 	
-	int N,t,max=-1;
-	cin >> N >> t;
+	long long hash_val=0;
 	
-	if(N == 1 && t == 1){
-		cout << 1;
+	hash_weight[0] = 1;
+	
+	for(int i = 1 ; i < L ; i++){
+		hash_weight[i] = (hash_weight[i-1]*31) % 1234567891;
 	}
-	else if(N == 1 && t == 1){
-		cout << 2;
+		
+	for(int i = 0 ; i < L ; i++){
+		hash_val += ((input[i]-'a'+1)*hash_weight[i])%1234567891;
 	}
+	
+	if(hash_val > ((long long)1234567891)*50)	cout << "error!\n";
+	cout << hash_val << endl;
 	
 	return 0;
 }
