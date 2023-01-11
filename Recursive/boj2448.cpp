607c462f10060312fa7be96b9eba4eeef559e031
정관용 <rhks7410dyd@naver.com>
2023-01-11 14:28:44 +0000

    new solved problem(230111)

--- Recursive/boj2448.cpp
+++ Recursive/boj2448.cpp
@@ -2,26 +2,26 @@
 
 using namespace std;
 
-int N,pattern = -1,rec_c=3;
+int N;
+int b_c[3100];
 char ln_in_3[4][6] = {"","*","* *","*****"};
 
-void print_star(int ln,int blank_count){
+void print_star(int ln,int blank_count,int p){
 	if(ln <= 3){
 		printf("%s",ln_in_3[ln]);
 		return;
 	}
 	
-	if(pattern <= 0){
-		pattern = 2*ln - 3;
-		rec_c *= 2;
-		if()
-	}
+	int t_p = p;
+	while(t_p >= ln - p)	t_p /= 2;
 	
-	print_star()
+	print_star(ln-p,b_c[ln-p],t_p);//여기서 0 부분을 채워주면됨!
 	
 	for(int i = 0 ; i < blank_count ; i++){
 		printf(" ");
 	}
+	
+	print_star(ln-p,b_c[ln-p],t_p);
 }
 
 int main(){
@@ -31,12 +31,28 @@ int main(){
 	
 	scanf("%d",&N);
 	
+	int p=3;
+	b_c[0] = 13;
 	for(int i = 1 ; i <= N ; i++){
+		if(2*p < i){
+			p *= 2;
+			b_c[i] = 2* p - 1;
+		}
+		else{
+			b_c[i] = b_c[i-1] - 2;
+		}
+		
+		//printf("%4d (%4d,%4d) : ",i,p,b_c[i]);
 		for(int j = 0 ; j < N - i ; j++){
 			printf(" ");
 		}
 		
-		print_star(i,pattern);
+		print_star(i,b_c[i],p);
+		
+		for(int j = 0 ; j < N - i ; j++){
+			printf(" ");
+		}
+		
 		printf("\n");
 	}
 }
