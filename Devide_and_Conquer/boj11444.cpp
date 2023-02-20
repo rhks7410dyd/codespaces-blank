20f963377343560153ca1db11d8c931100041b8e
정관용 <rhks7410dyd@naver.com>
2023-02-20 11:00:24 +0000

    unsolved problem(230220)

--- Devide_and_Conquer/boj11444.cpp
+++ Devide_and_Conquer/boj11444.cpp
@@ -1,6 +1,53 @@
 /*
 참조 : https://zzonglove.tistory.com/32
 머리 굴러갈 때 공부하고 풀어보기.
-행렬의 곱을 함수로 구현한 이후에 이를 이용해서 분할정복을 이용한 거듭제곱 문제를 풀면 됨
-다만 구현상에 이러한 방식과 DP를 이용해서 하는 두 가지 방식을 모두 해보는 것이 좋을것 같음.
+포스팅 된 글에서 말하는 4번째 방법을 사용하면 굳이 행렬의 곱을 구하지 않아도 쉽게 구현이 가능함. 이 방법을 사용해보기
+//시간 초과가 뜬다.
+vector<pair<int,int>> v 를 만들어서 num,fib_val 이런 식으로 페어를 활용해서 이미 만든 값을 활용하는 방식을 사용해서 다수의 저장을 방지하는 식으로 해보기.
+어차피 최대값이라고 해봤자 2^60 꼴이라 값의 개수가 많아봐야 몇천개를 안넘길 것 같음(확인이 필요하긴 함) 그래서 시간적으로는 여유가 있기 때문에 매번 탐색하는 방식으로 해도 크게 상관이
+없을 것 같음.
 */
+#include <iostream>
+
+using namespace std;
+
+int FIB[11] = {0,1,1,2,3,5,8,13,21,34,55};
+
+unsigned long long fib(unsigned long long n);
+
+int main(){
+	cin.tie(NULL);
+	cout.tie(NULL);
+	ios::sync_with_stdio(false);
+	
+	unsigned long long N;
+	cin >> N;
+	
+	unsigned long long res = fib(N);
+	
+	cout << res << '\n';
+	
+	return 0;
+}
+
+unsigned long long fib(unsigned long long n){
+	unsigned long long res;
+	if(!n)	return 0;
+	if(n < 11){
+		return FIB[n];
+	}
+	if(n&1){
+		unsigned long long F_k = fib(n/2+1);
+		unsigned long long F_k2 = fib(n/2);
+		F_k = F_k*F_k%1000000007;
+		F_k2 = F_k2*F_k2%1000000007;
+		res = (F_k + F_k2)%1000000007;
+	}
+	else{
+		unsigned long long F_k = fib(n/2-1);
+		unsigned long long F_k2 = fib(n/2);
+		F_k = (2*F_k+F_k2)%1000000007;
+		res = F_k*F_k2%1000000007;		
+	}
+	return res;
+}
