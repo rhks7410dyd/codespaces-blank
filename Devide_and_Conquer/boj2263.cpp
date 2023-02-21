6c574372bd95fc7bb113e280b5a98ed631f8d33e
정관용 <rhks7410dyd@naver.com>
2023-02-21 14:22:06 +0000

    unsolved problem(230221)

--- Devide_and_Conquer/boj2263.cpp
+++ Devide_and_Conquer/boj2263.cpp
@@ -1,3 +1,7 @@
+/*
+반례를 못찾고 있음.
+어디서 무슨 인덱스 실수가 났는지 확인해봐야됨.
+*/
 #include <iostream>
 #include <vector>
 
@@ -5,13 +9,13 @@ using namespace std;
 
 vector<int> inorder;
 vector<int> postorder;
-
-void recurfunc(int st,int ed);
+bool visit[100001];
+void recurfunc(int pst,int ped,int ist,int ied);
 
 int main(){
 	cin.tie(NULL);
 	cout.tie(NULL);
-	sync_with_stdio(false);
+	ios_base::sync_with_stdio(false);
 	
 	int n;
 	
@@ -24,18 +28,27 @@ int main(){
 	}
 	for(int i = 0 ; i < n ; i++){
 		cin >> t;
-		preorder.push_back(t);
+		postorder.push_back(t);
 	}
 	
-	recurfunc(0,n);
+	recurfunc(0,n,0,n);
+	
+	cout << '\n';
 	
 	return 0;
 }
 
-void recurfunc(int pst,int ped,int ist,int ied){
+void recurfunc(int ist,int ied,int pst,int ped){
+	if(visit[ped-1])	return;
+	visit[ped-1] = true;
 	cout << postorder[ped-1]  << ' ';
-	
+	//cout << "postorder[ped-1] : " << postorder[ped-1] << endl;
 	int root_node_index_in_inorder;
+	
+	if(pst+1 == ped){
+		return;
+	}
+	
 	for(int i = ist ; i < ied ; i++){
 		if(inorder[i] == postorder[ped-1]){
 			root_node_index_in_inorder = i;
@@ -43,5 +56,29 @@ void recurfunc(int pst,int ped,int ist,int ied){
 		}
 	}
 	
-	recurfunc()
+	int left_tree_size = root_node_index_in_inorder - ist;
+	int right_tree_size = ied - root_node_index_in_inorder-1;
+	
+	//여기서 한 쪽 노드가 없는 경우를 고려를 안했음! -> 완전이진트리인 경우만을 상정하고 문제를 풀었음
+	//cout << "left node func : " << ist << " , " << ist+left_tree_size << " , " << pst << " , " << pst+left_tree_size << endl;
+	//cout << "right node func : "  << ied-right_tree_size << " , " << ied << " , " << ped-right_tree_size-1 << " , " << ped - 1 << endl;
+	recurfunc(ist,ist+left_tree_size,pst,pst+left_tree_size);
+	recurfunc(ied-right_tree_size,ied,ped-right_tree_size-1,ped-1);
 }
+
+/*
+10
+4 2 1 6 5 7 3 10 9 8
+4 2 6 7 5 10 9 8 3 1
+1 2 4 3 5 6 7 8 9 10
+ln76에서 이상한 값이 잡히는 이유는 root_node_index_in_inorder 가 제대로 명명되지 않아서이다. 
+가장 단순하게 생각나는건 bool visit 어레이를 만들어서 이미 출력된 위치는 true로 바꿔놓고, return;으로 강제로 함수를 끝내는 방법이다.
+*/
+
+/*
+10
+5 3 6 2 7 4 9 8 10 1
+5 6 3 7 9 10 8 4 2 1
+1 2 3 5 6 4 7 8 9 10
+
+*/
