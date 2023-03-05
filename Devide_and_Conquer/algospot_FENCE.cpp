44b99e1358715bee95c8ddb7cb8176a4d00208a5
정관용 <rhks7410dyd@naver.com>
2023-03-05 10:53:36 +0000

    new solved problem(230305)

--- Devide_and_Conquer/algospot_FENCE.cpp
+++ Devide_and_Conquer/algospot_FENCE.cpp
@@ -1,10 +1,13 @@
 //백준 6549 문제와 동일한 문제임.
 #include <iostream>
 #include <stack>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 
-stack<pair<int,int>> st;//first -> height, second -> index number
+stack<int> st;
+vector<int> v;
 
 int main(){
 	cin.tie(NULL);
@@ -15,53 +18,55 @@ int main(){
 	scanf("%d",&C);
 	
 	for(int c = 0 ; c < C ; c++){
-		int max = -1;
 		scanf("%d",&N);
-		for(int i = 0 ; i < N ; i++){
+		
+		for(int i = 1 ; i <= N ; i++){
 			scanf("%d",&t);
-			if(st.empty() || st.top().first < t){
-				st.push({t,i});
-				continue;
-			}
-			
-			while(!st.empty() && st.top().first > t){
-				int dif = i - st.top().second;
-				int t_area = dif * st.top().first;
-				if(t_area > max){
-					max = t_area;
-				}
+			v.push_back(t);
+		}
+		
+		
+		int ans = 0;
+		v.push_back(0);
+		for(int i = 0 ; i <= N ; i++){
+			while(!st.empty() && v[st.top()] >= v[i]){
+				int c = st.top();
 				st.pop();
+				int width = st.empty() ? i : (i - st.top() - 1);
+				ans = max(ans,v[c]*width);//이 식이 맞고 기존의 내가 낸 식이 틀린 이유가 중요함
 			}
-			
-			if(!st.empty() && st.top().first == t){
-				continue;
-			}
-			
-			st.push({t,i});
+			st.push(i);
 		}
 		
-		while(st.size() > 1){
-			int dif = N - st.top().second;
-			int t_area = dif * st.top().first;
-			if(t_area > max){
-				max = t_area;
-			}
-			st.pop();
-		}
+		printf("%d\n",ans);
 		
-		int t_area = st.top().first * N;
-		if(t_area > max){
-			max = t_area;
-		}
-		cout << max << '\n';
-		
+		v.clear();
 		while(!st.empty()){
 			st.pop();
 		}
 	}
 	return 0;
 }
+
 /*
+6
+7
+2 1 4 5 1 3 3
+4
+1000 1000 1000 1000
 7
 7 1 5 9 6 7 3
+7
+1 4 4 4 4 1 1
+4
+1 8 2 2
+7
+15 2 2 2 2 2 1
+
+8
+4000
+20
+16
+8
+16
 */
