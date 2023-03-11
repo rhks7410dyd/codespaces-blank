aa0a4a5260fc2230a13e6bde7e2e0bc93530629b
정관용 <rhks7410dyd@naver.com>
2023-03-11 13:55:08 +0000

    unsolved problem(230311)

---
+++ Dynamic_Programming/boj17626.cpp
@@ -0,0 +1,28 @@
+/*
+종만북에서 기본적으로 DP는 재귀 함수를 활용한 완전 탐색 형태에서 변경되었다고 했기 때문에, 이 문제는 우선 재귀 함수를 활용해 완전 탐색하는 형태에서
+변형하는 방식으로 풀어보도록 했음
+*/
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int DP[50001];
+
+bool Find_set(vector<int>& a, int n,int c);
+
+int main(){
+	int N,ans;
+	cin >> N;
+	
+	for(int c = 1 ; c < 4 ; c++){
+		
+	}
+	
+	
+	return 0;
+}
+
+bool Find_set(vector<int>& a, int n,int c){
+	
+}
