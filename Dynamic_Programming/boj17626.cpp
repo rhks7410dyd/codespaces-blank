852b16c9ba795cac89b40af48afec402af479609
정관용 <rhks7410dyd@naver.com>
2023-03-13 13:55:02 +0000

    new solved problem(230313)

--- Dynamic_Programming/boj17626.cpp
+++ Dynamic_Programming/boj17626.cpp
@@ -3,26 +3,50 @@
 변형하는 방식으로 풀어보도록 했음
 */
 #include <iostream>
+#include <algorithm>
 #include <vector>
 
 using namespace std;
 
-int DP[50001];
-
-bool Find_set(vector<int>& a, int n,int c);
 
 int main(){
-	int N,ans;
+	int N,ans,temp;
 	cin >> N;
 	
-	for(int c = 1 ; c < 4 ; c++){
-		
+	vector<int> one;
+	vector<int> two;
+	vector<int> three;
+	bool find_ans = false;
+	for(int i = 1 ; i < 240 ; i++){
+		temp = i*i;
+		if(temp == N){
+			cout << 1 << "\n";
+			return 0;
+		}
+		one.push_back(i*i);
 	}
 	
+	for(int i = 0 ; i < one.size() ; i++){
+		for(int j = i ; j < one.size() ; j++){
+			temp = one[i] + one[j];
+			if(temp == N){
+				cout << 2 << "\n";
+				return 0;
+			}
+			two.push_back(one[i] + one[j]);
+		}
+	}
 	
+	for(int i = 0 ; i < one.size() ; i++){
+		for(int j = 0; j < two.size() ; j++){
+			temp = one[i] + two[j];
+			if(temp == N){
+				cout << 3 << "\n";
+				return 0;
+			}
+		}
+	}
+	
+	cout << 4 << "\n";
 	return 0;
-}
-
-bool Find_set(vector<int>& a, int n,int c){
-	
 }
