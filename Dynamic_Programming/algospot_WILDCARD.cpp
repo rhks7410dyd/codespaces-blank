2932d4c5de39852412e9272574c74459b08d13ff
정관용 <rhks7410dyd@naver.com>
2023-03-16 13:35:29 +0000

    unsolved problem(230316)

--- Dynamic_Programming/algospot_WILDCARD.cpp
+++ Dynamic_Programming/algospot_WILDCARD.cpp
@@ -1,17 +1,19 @@
 /*
 아래 주석 처리된 코드는 변형 전 완전 탐색 재귀 함수로 작성한 코드이다.
 */
-
 #include <iostream>
 #include <vector>
 #include <string>
+#include <algorithm>
 
 using namespace std;
 
 string wildcard;
+string word;
 vector<string> words;
+vector<string> correct_words;
 
-bool recursive(string word,int pos,int now_command);
+bool recursive(int pos,int now_command);
 
 int main(){
 	cin.tie(NULL);
@@ -33,34 +35,51 @@ int main(){
 		}
 		
 		for(int i = 0 ; i < word_c ; i++){
-			if(recursive(words[i],0,0)){
-				cout << words[i] << '\n';
+			word = words[i];
+			if(recursive(0,0)){
+				correct_words.push_back(word);
 			}
 		}
 		
+		sort(correct_words.begin(),correct_words.end());
+		
+		for(int i = 0 ; i < correct_words.size() ; i++){
+			cout << correct_words[i] << '\n';
+		}
+		
+		correct_words.clear();
 		words.clear();
 	}
 }
 
-bool recursive(string word,int pos,int now_command){	
-	if(pos >= word.size()){
-		return (now_command == wildcard.size())||(wildcard[now_command]=='*' && now_command == wildcard.size()-1);
+bool recursive(int pos,int now_command){
+	if(pos == word.size()){
+		if(wildcard[now_command] == '*' && now_command < wildcard.size() - 1)	return recursive(pos,now_command + 1);
+		return (now_command == wildcard.size())||(wildcard[now_command] == '*' && now_command == wildcard.size() - 1);
 	}
 
 	switch(wildcard[now_command]){
 		case '*':
-			return recursive(word,pos+1,now_command) || recursive(word,pos+1,now_command+1);
+			return recursive(pos+1,now_command) || recursive(pos+1,now_command+1);
 			break;
 		case '?':
-			return recursive(word,pos+1,now_command+1);
+			return recursive(pos+1,now_command+1);
 			break;
 		default:
-			if(word[pos] == wildcard[now_command])	return recursive(word,pos+1,now_command+1);
+			if(word[pos] == wildcard[now_command])	return recursive(pos+1,now_command+1);
 			break;
 	}
 	
 	return false;
 }
+
+/*
+1
+a**
+1
+a
+위의 테스트 케이스가 작동을 안함.
+*/
 
 /*
 #include <iostream>
@@ -103,7 +122,7 @@ int main(){
 	}
 }
 
-bool recursive(string word,int pos,int now_command){	
+bool recursive(string word,int pos,int now_command){
 	if(pos >= word.size()){
 		return (now_command == wildcard.size())||(wildcard[now_command]=='*' && now_command == wildcard.size()-1);
 	}
@@ -119,7 +138,7 @@ bool recursive(string word,int pos,int now_command){
 			if(word[pos] == wildcard[now_command])	return recursive(word,pos+1,now_command+1);
 			break;
 	}
-	
+
 	return false;
 }
 */
