1c0485e48314bedb34b671c8007c0b5095b1bf6f
정관용 <rhks7410dyd@naver.com>
2023-04-15 13:58:44 +0000

    new solved problem(230415)

--- Implementation/boj16236.cpp
+++ Implementation/boj16236.cpp
@@ -1,15 +1,15 @@
 //시간 초과가 뜸
 #include <iostream>
 #include <cstdlib>
-#include <algorithm>
+#include <cstring>
 #include <queue>
+#include <vector>
 
 using namespace std;
 
 int map[20][20];
 int N;
-bool visit[20][20];
-int f_r,f_c,s_r,s_c,shark_size=2,shark_eating_fish = 0;
+int plus_time,f_r,f_c,s_r,s_c,shark_size=2,shark_eating_fish = 0;
 int dir[4][2] = {{-1,0},{0,-1},{0,1},{1,0}};
 
 bool can_eat_fish();
@@ -34,7 +34,10 @@ int main(){
 	int time = 0;
 	
 	while(can_eat_fish()){
-		time += abs(s_c-f_c)+abs(s_r-f_r);
+		time += plus_time;
+		//cout << "shark position (" << s_c << ',' << s_r << ")\tfish position (" << f_c << ',' << f_r << ")\ttime : " << time << endl;
+		map[s_c][s_r] = 0;
+		map[f_c][f_r] = 0;
 		s_c = f_c;
 		s_r = f_r;
 		shark_eating_fish++;
@@ -42,42 +45,86 @@ int main(){
 			shark_size++;
 			shark_eating_fish = 0;
 		}
-		fill(&visit[0][0],&visit[19][20],false);
 	}
 	
 	cout << time << '\n';
+	
+	/*
+	for(int i = 0 ; i < N ; i++){
+		for(int j = 0 ; j < N ; j++){
+			cout << map[i][j] << ' ';
+		}
+		cout << endl;
+	}
+	*/
 	
 	return 0;
 }
 
 bool can_eat_fish(){
-	queue<pair<int,int>> q;
+	bool visit[20][20] = {false,};
+	queue<pair<pair<int,int>,int>> q;
 	
-	q.push({s_c,s_r});
+	q.push({{s_c,s_r},0});
 	visit[s_c][s_r] = true;
-	
+	//최단경로로 갈 수 없다는 사실을 고려할 것
 	while(!q.empty()){
-		auto now = q.front();
-		q.pop();
+		int qsize = q.size();
+		vector<pair<int,int>> v;
 		
-		for(int i = 0 ; i < 4 ; i++){
-			int next_r = now.second + dir[i][1];
-			int next_c = now.first + dir[i][0];
-			
-			if(next_r < 0 || next_c < 0 || next_r >= N || next_c >= N || visit[next_c][next_r])	continue;
-			
-			if(shark_size > map[next_c][next_r] && map[next_c][next_r] != 0){
-				f_c = next_c;
-				f_r = next_r;
-				return true;
+		for(int T = 0 ; T < qsize ; T++){
+			int now_c = q.front().first.first;
+			int now_r = q.front().first.second;
+			int now_t = q.front().second;
+			q.pop();
+
+			for(int i = 0 ; i < 4 ; i++){
+				int next_r = now_r + dir[i][1];
+				int next_c = now_c + dir[i][0];
+
+				if(next_r < 0 || next_c < 0 || next_r >= N || next_c >= N || visit[next_c][next_r])	continue;
+
+				if(shark_size > map[next_c][next_r] && map[next_c][next_r] != 0){
+					plus_time = now_t+1;
+					v.push_back({next_c,next_r});
+					continue;
+				}
+
+				if(shark_size == map[next_c][next_r] || map[next_c][next_r] == 0){
+					visit[next_c][next_r] = true;
+					q.push({{next_c,next_r},now_t+1});
+				}
 			}
-			
-			if(shark_size == map[next_c][next_r] || map[next_c][next_r] == 0){
-				visit[next_c][next_r] = true;
-				q.push({next_c,next_r});
+		}
+		
+		if(!v.empty()){
+			int best_hash_val = v[0].first * 100 + v[0].second;
+			int best_p=0;
+			for(int i = 0 ; i < v.size() ; i++){
+				if(best_hash_val > v[i].first * 100 + v[i].second){
+					best_hash_val = v[i].first * 100 + v[i].second;
+					best_p = i;
+				}
 			}
+			f_c = v[best_p].first;
+			f_r = v[best_p].second;
+			
+			return true;
 		}
 	}
 	
 	return false;
 }
+
+/*
+8
+1 0 1 0 1 0 1 0
+3 6 6 6 6 6 6 0
+0 0 0 0 0 0 0 0
+9 6 6 6 6 6 6 6
+0 0 0 0 0 0 0 0
+6 6 6 6 6 6 6 0
+2 2 2 2 2 2 3 3
+0 0 0 0 0 0 0 0
+
+*/
