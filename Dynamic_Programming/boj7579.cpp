6c65471386f6fe34f1ccc0864f731c09d265cb7f
정관용 <rhks7410dyd@naver.com>
2023-05-26 13:58:34 +0000

    new sovled problem(230526)

--- Dynamic_Programming/boj7579.cpp
+++ Dynamic_Programming/boj7579.cpp
@@ -5,25 +5,38 @@
 using namespace std;
 
 int N,M;
-int dp[100][10001];
-int cost[100];
-int mem[100];
+int dp[101][10001];
+int cost[101];
+int mem[101];
 
 int main(){
 	cin.tie(NULL);
 	cout.tie(NULL);
-	ios_base::sycn_with_stdio(false);
+	ios_base::sync_with_stdio(false);
 	
+	int sum = 0;
 	cin >> N >> M;
-	for(int i = 0 ; i < N ; i++){
+	for(int i = 1 ; i <= N ; i++){
 		cin >> mem[i];
 	}
-	for(int i = 0 ; i < N ; i++){
+	for(int i = 1 ; i <= N ; i++){
 		cin >> cost[i];
+		sum += cost[i];
 	}
 	
-	int ans = Solve(-1,);
+	for (int i = 1; i <= N; i++){
+		for (int j = 0; j <= sum; j++){
+			if (j - cost[i] >= 0)	dp[i][j] = max(dp[i][j], dp[i - 1][j - cost[i]] + mem[i]);
+			dp[i][j] = max(dp[i][j], dp[i - 1][j]);
+		}
+	}
 	
-	cout << ans << '\n';
+	for(int i = 0 ; i <= sum ; i++){
+		if(dp[N][i] >= M){
+			cout << i << '\n';
+			break;
+		}
+	}
+	
 	return 0;
 }
