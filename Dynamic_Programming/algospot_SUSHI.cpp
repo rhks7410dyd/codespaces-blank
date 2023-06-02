e3c93cd1d797335e9d2d63d89ed4bee74e4150d3
정관용 <rhks7410dyd@naver.com>
2023-06-02 14:17:58 +0000

    new sovled problem(230602)

--- Dynamic_Programming/algospot_SUSHI.cpp
+++ Dynamic_Programming/algospot_SUSHI.cpp
@@ -1,12 +1,11 @@
 #include <iostream>
-#include <vector>
 #include <algorithm>
 
 using namespace std;
 
-bool compare(pair<pair<int,int>,double>& a,pair<pair<int,int>,double>& b){
-	return a.second < b.second;
-}
+int n,m,price[20],pref[20];
+int c[201];
+int sushi();
 
 int main(){
 	cin.tie(NULL);
@@ -17,28 +16,36 @@ int main(){
 	scanf("%d",&C);
 	
 	while(C--){
-		int n,m;
 		scanf("%d %d",&n,&m);
-		vector<pair<int,int>> v[20];//지금까지 선호도 총합, 남은 예산
-		vector<pair<pair<int,int>,double>> prefer_val;
 		
-		int mon,prefer;
+		m/=100;
 		for(int i = 0 ; i < n ; i++){
-			scanf("%d %d",&mon,&prefer);
-			prefer_val.push_back({{mon,prefer},(double)mon/prefer});
+			scanf("%d %d",&price[i],&pref[i]);
+			price[i]/=100;
 		}
-		sort(prefer_val.begin(),prefer_val.end(),compare);//효율이 좋은 애가 앞으로 옴
 		
+		int ans = sushi();
 		
-		//얘를 굳이 예산을 다 채울 때까지 돌릴 이유가 없음. 다음 효율이 좋은 애가 무조건 효율이 더 좋아지는 시점 전까지만 계산을 돌리면 됨
-		//이 부분을 제대로 다듬어서 전처리 과정을 돌린 이후, 이전 초밥을 0개~그 갯수까지만 dp를 만들어 놓고, 다음 애에서는 거기까지만 보면 되게 만들면 됨
-		//처음 초밥만 끝까지 하면 됨(아닌 경우가 있을 수 있음. 이에 대해서도 판단 해봐야됨)
-		for(int i = 0 ; i < n ; i++){
-			
-		}
+		printf("%d\n",ans);
 	}
 	
 	return 0;
+}
+
+int sushi(){
+	int ret = 0;
+	c[0] = 0;
+	for(int budget = 1 ; budget <= m ; ++budget){
+		int cand = 0;
+		for(int dish = 0 ; dish < n ; ++dish){
+			if(budget >= price[dish]){
+				cand = max(cand,c[(budget-price[dish])%201]+pref[dish]);
+			}
+		}
+		c[budget%201] = cand;
+		ret = max(ret,cand);
+	}
+	return ret;
 }
 
 /*
