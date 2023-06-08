eb410b8c63d60f1b492fa554e1bb247868978acb
정관용 <rhks7410dyd@naver.com>
2023-06-08 14:20:58 +0000

    new sovled problem(230607)

--- Dynamic_Programming/boj1562.cpp
+++ Dynamic_Programming/boj1562.cpp
@@ -1,5 +1,6 @@
 /*
 1. 맨처음에 반복문으로 하는 곳 없애기
+=> 이건 할 수가 없는게 시작점을 9개로 따로 잡아야해서 어쩔 수 없이 반복문으로 시작을 설정해줘야됨
 2. 왜 안되는지 찾기
 */
 #include <iostream>
@@ -8,26 +9,26 @@
 
 using namespace std;
 
-int dp[1<<11][41];
+long long dp[1<<11][10][101];
 
-int Solve(int set,int now,int l);
+long long Solve(int set,int now,int l);
 
 int main(){
 	cin.tie(NULL);
 	cout.tie(NULL);
 	ios_base::sync_with_stdio(false);
-	
+
 	int N;
 	cin >> N;
 	
-	int ans=0;
+	long long ans=0;
 	
 	if(N < 10){
 		cout << ans << '\n';
 		return 0;
 	}
 	
-	fill(&dp[0][0],&dp[1<<11-1][41],-1);
+	fill(&dp[0][0][0],&dp[1<<11-1][9][101],-1);
 	
 	for(int i = 1 ; i < 10 ; i++){
 		ans += Solve(1<<i,i,N-1);
@@ -35,24 +36,33 @@ int main(){
 	}
 	
 	cout << ans << '\n';
-	
+
 	return 0;
 }
 
-int Solve(int set,int now,int l){
+long long Solve(int set,int now,int l){
 	if(l == 0){
 		if(set == 1023)	return 1;
 		return 0;
 	}
 	
-	int& ret = dp[set][l];
+	
+	long long& ret = dp[set][now][l];
 	if(ret != -1)	return ret;
 	ret = 0;
 	
-	if(now != 0)	ret += Solve((set|(1<<(now-1))),now-1,l-1);
+	if(now != 0){
+		int next_set = set|(1<<now-1);
+		ret += Solve(next_set,now-1,l-1);
+	}
 	ret%=MOD;
-	if(now != 9)	ret += Solve((set|(1<<(now+1))),now+1,l-1);
+	
+	if(now != 9){
+		int next_set = set|(1<<now+1);
+		ret += Solve(next_set,now+1,l-1);
+	}
 	ret%=MOD;
+	
 	
 	return ret;
 }
