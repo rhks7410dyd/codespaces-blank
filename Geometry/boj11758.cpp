9f992208ead0b56b96c6bc71ce3ef4e3b184f3c0
정관용 <rhks7410dyd@naver.com>
2023-06-17 13:59:56 +0000

    New solved problem(230617)

--- Geometry/boj11758.cpp
+++ Geometry/boj11758.cpp
@@ -5,6 +5,8 @@ using namespace std;
 
 int P[3][2];
 int V[2][2];
+bool exist_above_the_line();
+bool is_same_val(double a,int b);
 
 int main(){
 	cin.tie(NULL);
@@ -20,22 +22,39 @@ int main(){
 		V[i][1] = P[i+1][1] - P[i][1];
 	}
 	
+	double t = V[0][0]/sqrt(V[0][0]*V[0][0] + V[0][1]*V[0][1]);
 	double cos_val = (V[0][0]*V[1][0]+V[0][1]*V[1][1])/(sqrt(V[0][0]*V[0][0]+V[0][1]*V[0][1])*sqrt(V[1][0]*V[1][0]+V[1][1]*V[1][1]));
 	
-	if(cos_val == 1 || cos_val == -1){
+	
+	if(is_same_val(cos_val,1) || is_same_val(cos_val,-1)){
 		ans = 0;
 	}
 	else{
 		if(V[0][0] == 0){
 			//이것도 위아래 나눠야됨
-			ans = V[1][0] > 0 ? -1:1;
+			ans = (V[1][0]*V[0][1] > 0 ) ? -1:1;
 		}
-		else if(V[0][0] > 0){
-			
+		else if(t > 0){//V[1]의 위치벡터가 V[0]를 방향 벡터로 하는 직선보다 위에 있냐 아래에 있냐로 구분
+			if(exist_above_the_line())	ans = -1;
+			else	ans = 1;
 		}
 		else{
-			
+			if(!exist_above_the_line())	ans = 1;
+			else	ans = -1;
 		}
 	}
+	
+	cout << ans << '\n';
+	
 	return 0;
+}
+
+bool exist_above_the_line(){
+	if(V[0][1]*V[1][0] - V[0][0]*V[1][1] > 0)	return true;
+	return false;
+}
+
+bool is_same_val(double a,int b){
+	if(abs(a - b) <= 1e-10)	return true;
+	return false;
 }
