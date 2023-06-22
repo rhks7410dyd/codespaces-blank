9dda3f7be1f1f65e1851e0eeb9151e0bef638216
정관용 <rhks7410dyd@naver.com>
2023-06-22 14:15:07 +0000

    unsolved problem(230622)

---
+++ Geometry/boj16937.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int n,h,w,r[100],c[100];
+bool paper[101][101];
+
+int main(){
+	cin.tie(NULL);
+	cout.tie(NULL);
+	ios_base::sync_with_stdio(false);
+	
+	cin >> h >> w >> n;
+	for(int i = 0 ; i < n ; i++){
+		cin >> r[i] >> c[i]
+	}
+	
+	for(int i = 0 ; i < n ; i++){
+		for(int j = i+1 ; j < n ; j++){
+			
+		}
+	}
+	
+	return 0;
+}
