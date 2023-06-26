2b0273fc772bec5216ea663e0ab85452b789d609
정관용 <rhks7410dyd@naver.com>
2023-06-26 14:20:53 +0000

    unsolved problem(230626)

---
+++ Greedy/algospot_MINASTIRITH.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int n;
+double info[100][3];
+
+int main(){
+	cin.tie(NULL);
+	cout.tie(NULL);
+	ios_base::sycn_with_stdio(false);
+	
+	int c;
+	cin >> c;
+	while(c--){
+		cin >> n;
+		for(int i = 0 ; i < n ; i++){
+			cin >> info[0] >> info[1] >> info[2];
+		}
+		
+		
+	}
+	
+	return 0;
+}
