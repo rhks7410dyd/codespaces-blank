4a684343ad7f079ab96f3f61317451350ade57e6
정관용 <rhks7410dyd@naver.com>
2023-06-26 14:20:38 +0000

    new solved problem(230626)

--- Brute_Force/boj27172.cpp
+++ Brute_Force/boj27172.cpp
@@ -1,34 +1,42 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
-bool is_decimal[1000001];
-int n;
+vector<int> numbers;
+int scores[1000006];
+int cards[1000006];
 
-int main(){
-	cin.tie(NULL);
-	cout.tie(NULL);
+int main()
+{
 	ios_base::sync_with_stdio(false);
-	
-	cin >> n;
-	
-	vector<int> v;
-	int t;
-	for(int i = 0 ; i < n ; i++){
-		cin >> t;
-		v.push_back(t);
+	cin.tie(0);
+	cout.tie(0);
+
+	int N;
+	cin >> N;
+
+	for (int i = 0; i < N; i++){
+		int temp;
+		cin >> temp;
+
+		numbers.push_back(temp);
+		cards[temp] = 1;
 	}
-	
-	for(int i = 2 ; i <= 999 ; i++){
-		if(is_decimal[i])	continue;
-		
-		t = i;
-		while(t <= 1000000){
-			is_decimal[t] = true;
-			t += i;
+
+	for (int i = 0; i < N; i++){
+		for (int j = numbers[i] * 2; j < 1000001; j += numbers[i])
+		{
+			if (cards[j] == 1)
+			{
+				scores[numbers[i]]++;
+				scores[j]--;
+			}
 		}
 	}
-	
+
+	for (int i = 0; i < N; i++)	cout << scores[numbers[i]] << " ";
+
 	return 0;
 }
