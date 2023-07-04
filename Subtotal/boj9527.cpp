de447ea84c9ccfbdff7cef5791c00db9fca03014
정관용 <rhks7410dyd@naver.com>
2023-07-04 14:27:08 +0000

    unsolved problem(230704)

---
+++ Subtotal/boj9527.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	cin.tie(NULL);
+	cout.tie(NULL);
+	ios_base::sycn_with_stdio(false);
+	
+	unsigned long long a,b;
+	cin >> a >> b;
+	
+	//이거 이렇게하면 무조건 시간 초과임.
+	for(unsigned long long i = 1 ; i < b/2 + 1 ; i++){
+		
+	}
+	
+	return 0;
+}
