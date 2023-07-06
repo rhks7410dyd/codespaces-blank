2f4c6b9b6cd980a833368f3bad3532cdbd67f5e4
정관용 <rhks7410dyd@naver.com>
2023-07-06 14:03:31 +0000

    unsolved problem(230706)

--- Subtotal/boj9527.cpp
+++ Subtotal/boj9527.cpp
@@ -2,6 +2,8 @@
 
 using namespace std;
 
+long long subsum[55];
+
 int main(){
 	cin.tie(NULL);
 	cout.tie(NULL);
@@ -11,9 +13,12 @@ int main(){
 	cin >> a >> b;
 	
 	//이거 이렇게하면 무조건 시간 초과임.
-	for(unsigned long long i = 1 ; i < b/2 + 1 ; i++){
+	for(){
 		
 	}
 	
 	return 0;
 }
+
+//a_n = a_(n-1) + 2^(n-1);
+// b까지 총 개수 - a 까지 총 개수 느낌으로 풀면 됨
