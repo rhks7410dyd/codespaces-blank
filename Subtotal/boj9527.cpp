3155b1088d4a1423dff270e4fbbaddbc06a8347f
정관용 <rhks7410dyd@naver.com>
2023-07-07 03:44:04 +0000

    new solved problem(230707)

--- Subtotal/boj9527.cpp
+++ Subtotal/boj9527.cpp
@@ -1,24 +1,76 @@
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
 long long subsum[55];
-
+long long getAns(long long x);
 int main(){
 	cin.tie(NULL);
 	cout.tie(NULL);
-	ios_base::sycn_with_stdio(false);
+	ios_base::sync_with_stdio(false);
 	
-	unsigned long long a,b;
+	long long a,b;
 	cin >> a >> b;
 	
-	//이거 이렇게하면 무조건 시간 초과임.
-	for(){
-		
+	subsum[0] = 1;
+	for(int i = 1 ; i < 55 ; i++){
+		subsum[i] = 2*subsum[i-1] + (1LL<<i);
 	}
 	
+	long long ans = getAns(b) - getAns(a-1);
+	cout << ans << '\n';
 	return 0;
 }
 
+long long getAns(long long x) {
+	long long ret = x & 1;
+
+	for (int i = 54; i > 0; i--) {
+		if (x & (1LL << i)) {
+			ret += subsum[i - 1] + (x - (1LL << i) + 1);
+			x -= 1LL << i;
+		}
+	}
+
+	return ret;
+}
+
+
 //a_n = a_(n-1) + 2^(n-1);
 // b까지 총 개수 - a 까지 총 개수 느낌으로 풀면 됨
+
+/*
+0
+1			0!
+10
+11			1!
+100
+101
+110
+111			2!
+1000
+1001
+1010
+1011
+1100
+1101
+1110
+1111		3!
+10000
+10001
+10010
+10011
+10100
+10101
+10110
+10111
+11000
+11001
+11010
+11011
+11100
+11101
+11110
+11111		!
+*/
