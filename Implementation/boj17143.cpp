de687f7a761bed557e93ae4dc5a10d4ec47eb827
정관용 <rhks7410dyd@naver.com>
2023-07-09 06:55:23 +0000

    unsolved problem(230709)

--- Implementation/boj17143.cpp
+++ Implementation/boj17143.cpp
@@ -1,3 +1,6 @@
+/*
+어디서 뭐가 잘못된 건지 모르겟음
+*/
 #include <iostream>
 #include <utility>
 
@@ -17,10 +20,13 @@ struct sharkinfo{
 };
 
 struct sharkinfo info[10001];
-int map[101][101];
+int map[2][101][101];
 int R,C,M;
 
-void move_point(int num);
+void move_point(int num,int map_idx);
+void print_map(int map_idx);
+void moving_ver(int num,bool up);
+void moving_hor(int num,bool right);
 
 int main(){
 	cin.tie(NULL);
@@ -36,56 +42,67 @@ int main(){
 	
 	for(int i = 1 ; i <= M ; i++){
 		scanf("%d %d %d %d %d",&info[i].r,&info[i].c,&info[i].s,&info[i].d,&info[i].z);
-		map[info[i].r][info[i].c] = i;
+		map[0][info[i].r][info[i].c] = i;
 	}
 	
 	int ans = 0;
-	bool get_shark;
 	for(int i = 1 ; i <= C ; i++){
+		//printf("i %d ",i);
+		//print_map((i-1)%2);
 		for(int j = 1 ; j <= R ; j++){
-			if(map[i][j]){
-				ans++;
-				info[map[i][j]].catched = true;
+			if(map[(i-1)%2][j][i]){
+				ans+= info[map[(i-1)%2][j][i]].z;
+				info[map[(i-1)%2][j][i]].catched = true;
 				break;
 			}
 		}
 		
 		for(int j = 1 ; j <= M ; j++){
+			map[(i-1)%2][info[j].r][info[j].c] = 0;
 			if(!info[j].catched){
-				move_point(j);
+				move_point(j,i%2);
 			}
 		}
 	}
 	
+	printf("%d\n",ans);
+	
 	return 0;
 }
 
-void move_point(int num){
+void move_point(int num,int map_idx){
 	auto& now = info[num];
 	
-	if(now.s == 0){
-		return;
+	/*if(now.s == 1 || now.s == 2){
+		moving_ver(num,now.s%2);
+	}
+	else{
+		moving_hor(num,now.s%2);
+	}*/
+	if(now.s != 0){
+		switch(now.d){
+			case 1://up
+				break;
+			case 2://down
+				moving_ver(num,false);
+				break;
+			case 3://right
+				moving_hor(num,true);
+				break;
+			case 4://left
+				moving_hor(num,false);
+				break;
+		}
 	}
 	
-	switch(now.d){
-		case 1://up
-			now.r = moving_ver(now.r,now.s,true);
-			break;
-		case 2://down
-			now.r = moving_ver(now.r,now.s,false);
-			break;
-		case 3://right
-			now.c = moving_hor(now.c,now.s,true);
-			break;
-		case 4://left
-			now.c = moving_hor(now.c,now.s,false);
-			break;
+	if(map[map_idx][now.r][now.c] == 0){
+		map[map_idx][now.r][now.c] = num;
 	}
-	
-	if(map[now.r][now.c] != 0){
-		auto& same_pos_shark = info[map[now.r][now.c]];
+	else{
+		auto& same_pos_shark = info[map[map_idx][now.r][now.c]];
 		if(now.z > same_pos_shark.z){
 			same_pos_shark.catched = true;
+			map[map_idx][now.r][now.c] = num;
 		}
 		else{
 			now.catched = true;
@@ -93,14 +110,118 @@ void move_point(int num){
 	}
 }
 
-//이동해야되는 남은 거리량 > (가로or세로의 길이 - 1) 인 경우, 우항의 값만큼 dis에서 빼주고 bool 값을 뒤집음
-//이동해야되는 남은 거리량 <= (가로or세로의 길이 - 1) 일 때는 그냥 이만큼 이동시킴
-int moving_ver(int start,int dis,bool up){
-	while(dis > 0){
-		
+void print_map(int map_idx){
+	printf("print map\n");
+	for(int i = 1 ; i <= R ; i++){
+		for(int j = 1 ; j <= C ; j++){
+			printf("%d ",map[map_idx][i][j]);
+		}
+		printf("\n");
 	}
 }
 
-int moving_hor(int start,int dis,bool right){
+//이동해야되는 남은 거리량 > (가로or세로의 길이 - 1) 인 경우, 우항의 값만큼 dis에서 빼주고 bool 값을 뒤집음
+//이동해야되는 남은 거리량 <= (가로or세로의 길이 - 1) 일 때는 그냥 이만큼 이동시킴
+void moving_ver(int num,bool up){
+	auto& now = info[num];
+	int start = now.r;
+	int dis = now.s;
+	if(dis == 0){
+		return;
+	}
 	
+	if(up && start - dis > 0){
+		start = start - dis;
+	}
+	else if(!up && start + dis <= R){
+		start = start + dis;
+	}
+	else{//한번은 턴을 하는 경우
+		if(up){
+			dis -= (start-1);
+			start = 1;
+			up = false;
+		}
+		else{
+			dis -= (R-start);
+			start =R;
+			up = true;
+		}
+		
+		while(dis >= R){
+			if(up){
+				dis -= (start-1);
+				start = 1;
+				up = false;
+			}
+			else{
+				dis -= (R-start);
+				start = R;
+				up = true;
+			}
+		}
+		if(up){
+			now.d = 1;
+			start = R-dis;
+		}
+		else{
+			now.d = 2;
+			start = 1+dis;
+		}
+	}
+	
+	now.r = start;
+	return;
+}
+
+void moving_hor(int num,bool right){
+	auto& now = info[num];
+	int start = now.c;
+	int dis = now.s;
+	if(dis == 0){
+		return;
+	}
+	
+	if(!right && start - dis > 0){
+		start = start - dis;
+	}
+	else if(right && start + dis <= C){
+		start = start + dis;
+	}
+	else{//한번은 턴을 하는 경우
+		if(!right){
+			dis -= (start-1);
+			start = 1;
+			right = true;
+		}
+		else{
+			dis -= (C-start);
+			start =C;
+			right = false;
+		}
+		
+		while(dis >= C){
+			if(!right){
+				dis -= (start-1);
+				start = 1;
+				right = true;
+			}
+			else{
+				dis -= (C-start);
+				start = C;
+				right = false;
+			}
+		}
+		if(right){
+			now.d = 3;
+			start = 1+dis;
+		}
+		else{
+			now.d = 4;
+			start = C-dis;
+		}
+	}
+	
+	now.c = start;
+	return;
 }
--- temparary_code.cpp
+++ temparary_code.cpp
@@ -1,104 +1,51 @@
-#include <iostream>
-#include <stack>
-#include <random>
-
-using namespace std;
-
-int n,b;
-string ans = "";
-string output = "";
-void My_solve(){
-	int N = n;
-	int B = b;
-	int tmp;
-	stack <int> s;
-	while(B<=N){
-		tmp=N%B;
-		N/=B;
-		s.push(tmp);
+void moving_hor(int num,bool right){
+	auto& now = info[num];
+	int start = now.c;
+	int dis = now.s;
+	if(dis == 0){
+		return;
 	}
-	s.push(N);
-	char alpha;
-	int digit = s.size();
-	for(int i = 0 ; i < digit ; i ++){
-		tmp=s.top();
-		s.pop();
-		if(tmp>9){
-			alpha=tmp-10+'A';
-			ans += alpha;
+	
+	if(!right && start - dis > 0){
+		start = start - dis;
+	}
+	else if(right && start + dis <= C){
+		start = start + dis;
+	}
+	else{//한번은 턴을 하는 경우
+		if(!right){
+			dis -= (start-1);
+			start = 1;
+			right = true;
 		}
 		else{
-			ans += temp;
-		}
-	}
-}
-
-
-
-int main(){
-	
-	cin >> N >> B;
-	
-	
-	return 0;
-}
-
-int main(void)
-{
-	char n[1001] = {0};
-	int a, b, x, count = 0;
-	
-	scanf("%d %d", &a, &b);
-	
-	if (b >= 10)
-	{
-		for (int i = 0; a != 0; i++)       // i번 만큼 count
-		{
-			if (a/b >= 0)
-			{
-				n[i] = a%b;
-				a = a/b;
-				count+=1;
-			}
-		}
-		for (int j = 0; j < count/2; j++)  // 배열을 순서대로 정렬
-		{
-			int temp = n[j];
-			n[j] = n[(count-1)-j];
-			n[(count-1)-j] = temp;
+			dis -= (C-start);
+			start =C;
+			right = false;
 		}
 		
-		for (int k = 0; k < count; k++)
-		{
-			if(n[k] >= 10)	n[k] = n[k] + 'A' - 10;
-			else n[k] = n[k] + '0';
-		}
-	}
-
-	else if (b <= 9)
-	{
-		for (int i = 0; a != 0; i++)
-		{
-			if (a/b >= 0)                  // 실행된 i번 만큼 count +1
-			{
-				n[i] = a%b;
-				a = a/b;
-				count+=1;
+		while(dis >= C){
+			if(!right){
+				dis -= (start-1);
+				start = 1;
+				right = true;
+			}
+			else{
+				dis -= (C-start);
+				start = C;
+				right = false;
 			}
 		}
-		for (int j = 0; j < count/2; j++)  // 배열을 순서대로 정렬
-		{
-			int temp = n[j];
-			n[j] = n[(count-1)-j];
-			n[(count-1)-j] = temp;
+		if(right){
+			now.d = 3;
+			start = 1+dis;
 		}
-		
-		for(int i = 0 ; i < count ; i++){
-			n[i] = n[i] + '0';
+		else{
+			now.d = 4;
+			start = C-dis;
 		}
 	}
 	
-    printf("%s", n);	// 10진수 이상 출력  
-	
-    return 0;
+	now.c = start;
+	return;
 }
