439bd0addce1d5b407d700fcc97eb8e29b98365d
정관용 <rhks7410dyd@naver.com>
2023-07-14 14:27:44 +0000

    unsolved problem(230714)

--- Backtracking/boj1799.cpp
+++ Backtracking/boj1799.cpp
@@ -2,18 +2,19 @@
 동일한 열에 두 개의 비숍을 둘 수 있기 때문에 2차원 배열로 선언했어야됐는데..귀찮아서 나중에 하고 싶음
 */
 #include <iostream>
-#include <queue>
 #include <vector>
 #include <algorithm>
+#define INF -987654321
 
 using namespace std;
 
 int n;
 int dir[4][2] = {{1,1},{1,-1},{-1,1},{-1,-1}};
 int map[10][10];
-int pos[10];
+int pos[10][10];
 
-int get_ans(int idx);
+int get_ans(int c,int r,bool put_bishop);
+bool can_put_bishop(int r,int c);
 
 int main(){
 	cin.tie(NULL);
@@ -25,44 +26,67 @@ int main(){
 	for(int i = 0 ; i < n ; i++){
 		for(int j = 0 ; j < n ; j++){
 			cin >> map[i][j];
-			pos[j] = -1;
 		}
 	}
 	
-	int ans = get_ans(0);
-	
+	int ans = get_ans(0,0,false);
+	if(map[0][0] == 1){
+		pos[0][0] = 1;
+		ans = min(ans,get_ans(0,0,true)+1);
+	}
 	
 	cout << ans << '\n';
 	
 	return 0;
 }
 
-int get_ans(int idx){
-	if(idx == n){
+/*
+100개의 모든 칸에 대하여 그 칸에 비숍이 들어간 경우와 들어가지 않은 경우를 모두 재귀로 불러들임.
+그래서 그 칸에 비숍을 두지 못한다면 INF를 즉시 리턴하도록 설정
+dp를 이용해도 나쁘지 않을 거 같기도 하다고 느꼇지만, 필요한 용량이 너무 커져서 안됨
+*/
+//여기 계산 어디서 하나 더 해지는 기분
+//https://www.acmicpc.net/board/view/103135
+//위 링크 반례에서 시간 복잡도를 망쳐버림
+int get_ans(int r,int c,bool put_bishop){
+	int ret = INF;
+	if(r == n - 1 && c == n-1){
+		if(put_bishop){
+			if(can_put_bishop(r,c))	return 0;
+			else	return INF;
+		}
 		return 0;
 	}
 	
-	bool point_pos[10];
-	for(int i = 0 ; i < idx ; i++){
-		if(pos[i] == -1)	continue;
-		if(pos[i] + idx - i < 10){
-			point_pos[pos[i] + idx - i] = true;
-		}
-		if(pos[i] - idx + i >= 0){
-			point_pos[pos[i] - idx + i] = true;
-		}
+	if(put_bishop && !can_put_bishop(r,c)){
+		return INF;
 	}
 	
-	int ret = get_ans(idx+1);
-	bool can_put_bishop = false;
-	for(int i = 0 ; i < n ; i++){
-		if(map[idx][i] == 1 && !point_pos[i]){
-			can_put_bishop = true;
-			pos[idx] = i;
-			ret = max(ret,get_ans(idx+1)+1);
-			pos[idx] = -1;
-		}
+	int nr = r, nc = c + 1;
+	if(nc == n){
+		nr = r + 1;
+		nc = 0;
 	}
+	
+	if(map[nr][nc] == 1){
+		pos[nr][nc] = 1;
+		ret = max(ret,get_ans(nr,nc,true)+1);
+		pos[nr][nc] = 0;
+	}
+	ret = max(ret,get_ans(nr,nc,false));
 	
 	return ret;
+}
+
+bool can_put_bishop(int r,int c){
+	for(int d = 0 ; d < 4 ; d++){
+		int nr = r;
+		int nc = c;
+		while(nr >= 0 && nr < n && nc >= 0 && nc < n){
+			nr += dir[d][0];
+			nc += dir[d][1];
+			if(pos[nr][nc])	return false;
+		}
+	}
+	return true;
 }
