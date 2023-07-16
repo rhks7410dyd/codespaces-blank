3c07f6fa5f96128e81e86cd719e64464fc332e49
정관용 <rhks7410dyd@naver.com>
2023-07-16 14:25:32 +0000

    unsolved problem(230716)

--- Backtracking/boj1799.cpp
+++ Backtracking/boj1799.cpp
@@ -13,7 +13,8 @@ int dir[4][2] = {{1,1},{1,-1},{-1,1},{-1,-1}};
 int map[10][10];
 int pos[10][10];
 
-int get_ans(int c,int r,bool put_bishop);
+int get_ans_W(int r,int c,bool put_bishop);
+int get_ans_B(int r,int c,bool put_bishop);
 bool can_put_bishop(int r,int c);
 
 int main(){
@@ -29,51 +30,101 @@ int main(){
 		}
 	}
 	
-	int ans = get_ans(0,0,false);
-	if(map[0][0] == 1){
-		pos[0][0] = 1;
-		ans = min(ans,get_ans(0,0,true)+1);
+	if(n == 1){
+		cout << map[0][0] << '\n';
+		return 0;
 	}
 	
+	int ans_w = get_ans_W(0,0,false);
+	if(map[0][0] == 1){
+		pos[0][0] = 1;
+		ans_w = max(ans_w,get_ans_W(0,0,true)+1);
+		pos[0][0] = 0;
+	}
+	
+	int ans_b=0;
+	ans_b = get_ans_B(0,1,false);
+	if(map[0][1] == 1){
+		pos[0][1] = 1;
+		ans_b = max(ans_b,get_ans_B(0,1,true)+1);
+		pos[0][1] = 0;
+	}
+
+	//cout << ans_w << ' ' << ans_b << '\n';
+	
+	int ans = ans_w + ans_b;
 	cout << ans << '\n';
 	
 	return 0;
 }
 
-/*
-100개의 모든 칸에 대하여 그 칸에 비숍이 들어간 경우와 들어가지 않은 경우를 모두 재귀로 불러들임.
-그래서 그 칸에 비숍을 두지 못한다면 INF를 즉시 리턴하도록 설정
-dp를 이용해도 나쁘지 않을 거 같기도 하다고 느꼇지만, 필요한 용량이 너무 커져서 안됨
-*/
-//여기 계산 어디서 하나 더 해지는 기분
-//https://www.acmicpc.net/board/view/103135
-//위 링크 반례에서 시간 복잡도를 망쳐버림
-int get_ans(int r,int c,bool put_bishop){
+
+int get_ans_W(int r,int c,bool put_bishop){
 	int ret = INF;
-	if(r == n - 1 && c == n-1){
-		if(put_bishop){
-			if(can_put_bishop(r,c))	return 0;
-			else	return INF;
+	bool is_possible = can_put_bishop(r,c);
+	if(r == n - 1 && (c == n-1 || c == n-2)){
+		if(put_bishop && !is_possible){
+			return INF;
 		}
 		return 0;
 	}
 	
-	if(put_bishop && !can_put_bishop(r,c)){
+	if(put_bishop && !is_possible){
 		return INF;
 	}
 	
-	int nr = r, nc = c + 1;
-	if(nc == n){
+	int nr = r, nc = c + 2;
+	if(nc >= n){
 		nr = r + 1;
-		nc = 0;
+		if(nr%2){
+			nc = 1;
+		}
+		else{
+			nc = 0;
+		}
 	}
 	
 	if(map[nr][nc] == 1){
 		pos[nr][nc] = 1;
-		ret = max(ret,get_ans(nr,nc,true)+1);
+		ret = max(ret,get_ans_W(nr,nc,true)+1);
 		pos[nr][nc] = 0;
 	}
-	ret = max(ret,get_ans(nr,nc,false));
+	ret = max(ret,get_ans_W(nr,nc,false));
+	
+	return ret;
+}
+
+int get_ans_B(int r,int c,bool put_bishop){
+	int ret = INF;
+	bool is_possible = can_put_bishop(r,c);
+	if(r == n - 1 && (c == n-1 || c == n-2)){
+		if(put_bishop && !is_possible){
+			return INF;
+		}
+		return 0;
+	}
+	
+	if(put_bishop && !is_possible){
+		return INF;
+	}
+	
+	int nr = r, nc = c + 2;
+	if(nc >= n){
+		nr = r + 1;
+		if(nr%2){
+			nc = 0;
+		}
+		else{
+			nc = 1;
+		}
+	}
+	
+	if(map[nr][nc] == 1){
+		pos[nr][nc] = 1;
+		ret = max(ret,get_ans_B(nr,nc,true)+1);
+		pos[nr][nc] = 0;
+	}
+	ret = max(ret,get_ans_B(nr,nc,false));
 	
 	return ret;
 }
@@ -90,3 +141,20 @@ bool can_put_bishop(int r,int c){
 	}
 	return true;
 }
+
+/*
+왜 내가 본 테케중에 10일 때인 이것만 안될까?...
+10
+1 1 1 1 1 1 1 1 1 1
+1 1 1 1 1 1 1 1 1 1
+1 1 1 1 1 1 1 1 1 1
+1 1 1 1 1 1 1 1 1 1
+1 1 1 1 1 1 1 1 1 1
+1 1 1 1 1 1 1 1 1 1
+1 1 1 1 1 1 1 1 1 1
+1 1 1 1 1 1 1 1 1 1
+1 1 1 1 1 1 1 1 1 1
+1 1 1 1 1 1 1 1 1 1
+
+ans : 18
+*/
